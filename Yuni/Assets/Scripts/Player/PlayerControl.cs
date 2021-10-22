b7911fb3d43083bc229473b7ab11d3bf4c86bcde
Charuna Piyumantha <83096173+Charuna99@users.noreply.github.com>
2021-10-22 19:55:54 +0530

    Player Control Basics

---
+++ Yuni/Assets/Scripts/Player/PlayerControl.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    private Rigidbody _rBody;
+
+    private float verticalThrust = 25f;
+    private float horizontalThrust = 15f;
+
+    private void Awake()
+    {
+        _rBody = GetComponent<Rigidbody>();
+    }
+
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+        ProcessInput();
+
+    }//Update
+
+    private void ProcessInput()
+    {
+
+        if (Input.GetKey(KeyCode.Space)) // Thrust(Verticle Control)
+        {
+            _rBody.AddForce(Vector3.up * verticalThrust);
+        }
+
+        //Horizontal Contrlon and Rotation Control
+        if (Input.GetKey(KeyCode.A))
+        {
+
+            _rBody.AddForce(-Vector3.right * horizontalThrust);
+
+            transform.Rotate(Vector3.forward * 1);
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+
+            _rBody.AddForce(Vector3.right * horizontalThrust);
+
+            transform.Rotate(-Vector3.forward * 1);
+
+        }
+
+        //balance
+        else if (transform.eulerAngles.z < 15)
+        {
+            transform.Rotate(-Vector3.forward*0.6f);
+        }
+
+        else if (transform.eulerAngles.z > 345)
+        {
+            transform.Rotate(Vector3.forward*0.6f);
+        }
+
+
+        //lock
+        if ((transform.eulerAngles.z > 10) && (transform.eulerAngles.z < 180))
+        {
+            transform.eulerAngles = new Vector3(0, 0, 10);
+        }
+
+
+        if ((transform.eulerAngles.z < 350) && (transform.eulerAngles.z > 180))
+        {
+            transform.eulerAngles = new Vector3(0, 0, 350);
+        }
+
+
+    }
+
+}
